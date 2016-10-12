2082d052d55a1e021dcddde4e30bf2f7e0bc7963
Zachary Terwort <zterwort@ksu-wireless-4-252.campus.ksu.edu>
2016-10-12 15:19:57 -0500

    end class

--- bundle.js
+++ bundle.js
@@ -3,6 +3,7 @@
 
 /* Classes */
 const Game = require('./game');
+const Vector = require('./vector');
 
 /* Global variables */
 var canvas = document.getElementById('screen');
@@ -246,6 +247,36 @@ function update(elapsedTime) {
     }
   });
 
+  collisions.forEach(function(pair){
+    var collisionNormal = {
+      x: pair.a.position.x - pair.b.position.x,
+      y: pair.a.position.y - pair.b.position.y
+    }
+    //calcluate the overlap between balls
+    var overlap = 32 - Vector.magnitude(collisionNormal);
+    var collisionNormal = Vector.normalize(collisionNormal);
+    pair.a.position.x += collisionNormal.x * overlap / 2;
+    pair.a.position.y += collisionNormal.y * overlap / 2;
+    pair.b.position.x -= collisionNormal.x * overlap / 2;
+    pair.b.position.y -= collisionNormal.y * overlap / 2;
+    // Rotate the problem space so that the normal
+    // of collision lies along the x-axis
+    var angle = Math.atan2(collisionNormal.y, collisionNormal.x);
+    var a = Vector.rotate(pair.a.velocity, angle);
+    var b = Vector.rotate(pair.b.velocity, angle);
+    // Solve the collision along the x-axis
+    var hold = a.x;
+    a.x = b.x;
+    b.x = hold;
+    //Rotate the problem space back to world space
+    a = Vector.rotate(a, -angle);
+    b = Vector.rotate(b, -angle);
+    pair.a.velocity.x = a.x;
+    pair.a.velocity.y = a.y;
+    pair.b.velocity.x = b.x;
+    pair.b.velocity.y = b.y;
+  });
+
   // TODO: Process ball collisions
 }
 
@@ -305,7 +336,7 @@ function render(elapsedTime, ctx) {
   ctx.beginPath();
 }
 
-},{"./game":2}],2:[function(require,module,exports){
+},{"./game":2,"./vector":3}],2:[function(require,module,exports){
 "use strict";
 
 /**
@@ -361,6 +392,34 @@ Game.prototype.loop = function(newTime) {
 
   // Flip the back buffer
   this.frontCtx.drawImage(this.backBuffer, 0, 0);
+}
+
+},{}],3:[function(require,module,exports){
+module.exports = exports = {
+  rotate: rotate,
+  dotProduct: dotProduct,
+  magnitude: magnitude,
+  normalize: normalize
+}
+
+function rotate(a, angle){
+  return {
+    x: a.x * Math.cos(angle) - a.y * Math.sin(angle),
+    y: a.x * Math.sin(angle) + a.y * Math.cos(angle)
+  }
+}
+
+function dotProduct(a,b) {
+  return a.x * b.x + a.y * b.y
+}
+
+function magnitude(a){
+  return Math.sqrt(a.x * a.x + a.y * a.y);
+}
+
+function normalize(a){
+  var mag = magnitude(a);
+  return { x: a.x / mag, y: a.y / mag };
 }
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 /* Classes */
 const Game = require('./game');
+const Vector = require('./vector');
 
 /* Global variables */
 var canvas = document.getElementById('screen');
@@ -243,6 +244,36 @@ function update(elapsedTime) {
       // Push the colliding pair into our collisions array
       collisions.push(pair);
     }
+  });
+
+  collisions.forEach(function(pair){
+    var collisionNormal = {
+      x: pair.a.position.x - pair.b.position.x,
+      y: pair.a.position.y - pair.b.position.y
+    }
+    //calcluate the overlap between balls
+    var overlap = 32 - Vector.magnitude(collisionNormal);
+    var collisionNormal = Vector.normalize(collisionNormal);
+    pair.a.position.x += collisionNormal.x * overlap / 2;
+    pair.a.position.y += collisionNormal.y * overlap / 2;
+    pair.b.position.x -= collisionNormal.x * overlap / 2;
+    pair.b.position.y -= collisionNormal.y * overlap / 2;
+    // Rotate the problem space so that the normal
+    // of collision lies along the x-axis
+    var angle = Math.atan2(collisionNormal.y, collisionNormal.x);
+    var a = Vector.rotate(pair.a.velocity, angle);
+    var b = Vector.rotate(pair.b.velocity, angle);
+    // Solve the collision along the x-axis
+    var hold = a.x;
+    a.x = b.x;
+    b.x = hold;
+    //Rotate the problem space back to world space
+    a = Vector.rotate(a, -angle);
+    b = Vector.rotate(b, -angle);
+    pair.a.velocity.x = a.x;
+    pair.a.velocity.y = a.y;
+    pair.b.velocity.x = b.x;
+    pair.b.velocity.y = b.y;
   });
 
   // TODO: Process ball collisions
